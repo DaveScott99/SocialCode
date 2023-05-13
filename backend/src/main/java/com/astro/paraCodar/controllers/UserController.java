a101203092ffc1d8ae74923b8587abebbcfda385
DaviSantos <101915085+DaveScott99@users.noreply.github.com>
2023-05-12 23:01:00 -0300

    User CRUD

--- backend/src/main/java/com/astro/paraCodar/controllers/UserController.java
+++ backend/src/main/java/com/astro/paraCodar/controllers/UserController.java
@@ -1,10 +1,18 @@
 package com.astro.paraCodar.controllers;
 
+import java.net.URI;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.astro.paraCodar.entities.User;
 import com.astro.paraCodar.service.UserService;
@@ -16,9 +24,31 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
-	@PostMapping
-	public void insert(@RequestBody User user) {
-		userService.insert(user);
+	@GetMapping
+	public ResponseEntity<List<User>> findAll(){
+		List<User> users = userService.findAll();
+		return ResponseEntity.ok().body(users);
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@RequestParam Long id){
+		User user = userService.findById(id);
+		return ResponseEntity.ok().body(user);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		User user = userService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).body(user);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@RequestParam Long id, @RequestBody User newUser){
+		User user = userService.update(id, newUser);
+		return ResponseEntity.ok().body(user);
+	}
+	
+	
 	
 }
--- backend/src/main/java/com/astro/paraCodar/service/UserService.java
+++ backend/src/main/java/com/astro/paraCodar/service/UserService.java
@@ -1,10 +1,14 @@
 package com.astro.paraCodar.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.astro.paraCodar.entities.User;
 import com.astro.paraCodar.repositories.UserRepository;
+
 
 @Service
 public class UserService {
@@ -12,8 +16,32 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepository;
 	
-	public void insert(User user) {
-		userRepository.save(user);		
+	public List<User> findAll(){
+		List<User> users = userRepository.findAll();
+		return users;
+	}
+	
+	@Transactional(readOnly = true)
+	public User findById(Long id){
+		User user = userRepository.findById(id).get();
+		return user;
+	}
+	
+	@Transactional
+	public User insert(User user) {
+		return userRepository.save(user);		
+	}
+	
+	
+	public User update(Long id, User newUser){
+		User oldUser = userRepository.getReferenceById(id);
+		updateData(newUser, oldUser);
+		return userRepository.save(newUser);
+	}
+	
+	private void updateData(User newUser, User oldUser) {
+		newUser.setName(oldUser.getName());
+		newUser.setEmail(oldUser.getEmail());
 	}
 	
 }
