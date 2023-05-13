7097149e182e52d94a61ae6fad482cd1c6f34384
Davi Santos <davicalixto2077@gmail.com>
2023-05-12 22:10:34 -0300

    back-end created

---
+++ backend/src/test/java/com/astro/paraCodar/ParaCoderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astro.paraCodar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParaCoderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
