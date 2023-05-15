ab6ec3c4f5972a994713fabedd465df316cd6cb4
DaviSantos <101915085+DaveScott99@users.noreply.github.com>
2023-05-14 22:55:51 -0300

    ADD ControllerExceptions - BACKEND

---
+++ backend/src/main/java/com/astro/paraCodar/controllers/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,42 @@
+package com.astro.paraCodar.controllers.exceptions;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.astro.paraCodar.services.exceptions.ControllerNotFoundException;
+import com.astro.paraCodar.services.exceptions.DatabaseException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+	@ExceptionHandler(ControllerNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(ControllerNotFoundException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Recurso não encontrado");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Exceção do banco de dados");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+}
