194c1560481fa570ca85d5d5b6fa6c71b10bdbf4
DaviSantos <101915085+DaveScott99@users.noreply.github.com>
2023-05-29 13:16:28 -0300

    UPDATE FormatDate of the Post - FRONTEND

--- frontend/src/utils/FormatDateInfo.jsx
+++ frontend/src/utils/FormatDateInfo.jsx
@@ -1,22 +1,28 @@
 import moment from "moment/moment";
 
 const dateFormat = (datePost) => {
-    const postDate = moment(datePost);
 
     const now = moment();
+
+    const postDate = moment(datePost).format();
 
     const diff = moment.duration(now.diff(postDate))
 
     const days = diff.days();
     const hours = diff.hours();
+    const minute = diff.minutes();
+
+    // return days + "d " + hours + "h " + minute + "m ";
 
     if (days > 0) {
         return days + " d";
     }
-    else if (days === 0) {
+    else if (minute <= 59 && hours <= 0) {
+        return minute + " min"
+    }
+    else{
         return hours + " h";
     }
-    
 }
 
 export {dateFormat};
